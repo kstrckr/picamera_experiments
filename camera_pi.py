d307e9f607d7b2772067c1fc7c5245c0f170f6b5
Kurt Strecker <ksphotopro@gmail.com>
2017-11-20 00:30:48 -0500

    update 1

--- camera_pi.py
+++ camera_pi.py
@@ -1,5 +1,6 @@
 from picamera import PiCamera
 from time import sleep
+from datetime import time 
 
 
 with PiCamera() as camera:
@@ -7,8 +8,10 @@ with PiCamera() as camera:
     camera.resolution = (3280, 2464)
     camera.exposure_mode = 'night'
 
-    for i in range(5):
+    for i in range(1):
         sleep(0.5)
-        shutter_speed = camera.shutter_speed
-        camera.annotate_text = shutter_speed
+        shutter_speed_microseconds = camera.exposure_speed
+        shutter_speed = time(microsecond=shutter_speed_microseconds) 
+        print(shutter_speed.strftime('%S.%f'))
+        camera.annotate_text = shutter_speed.strftime('%S.%f') 
         camera.capture('./image%s.jpg' % i)
